596d67e3cc55082257e8b6621940637c6746c776
badaouiadampro-cpu <badaoui.adam.pro@gmail.com>
2026-02-02 10:53:44 +0100

    Update navbar.js

--- components/screen/navbar.js
+++ components/screen/navbar.js
@@ -13,16 +13,7 @@ export default class Navbar extends Component {
 
 	render() {
 		return (
-			<div className="main-navbar-vp absolute top-0 right-0 w-screen shadow-md flex flex-nowrap justify-between items-center bg-ub-grey text-ubt-grey text-sm select-none z-50">
-				<div
-					tabIndex="0"
-					className={
-						'pl-3 pr-3 outline-none transition duration-100 ease-in-out border-b-2 border-transparent focus:border-ubb-orange py-1 '
-					}
-				>
-					Activities
-				</div>
-				<div
+			<div
 					tabIndex="0"
 					className={
 						'pl-2 pr-2 text-xs md:text-sm outline-none transition duration-100 ease-in-out border-b-2 border-transparent focus:border-ubb-orange py-1'
