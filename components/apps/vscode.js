f808b062cc505552d40700ebf50d61864f52c023
badaouiadampro-cpu <badaoui.adam.pro@gmail.com>
2026-02-11 11:11:29 +0100

    Update vscode.js

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default function VsCode() {
     return (
-        <iframe src="https://github.com/badaouiadampro-cpu/" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
-        // this is not my work, but it's amazing!
-        //hai
-        // Here is the link to the original repo: https://github.com/conwnet/github1s
+        <iframe 
+            src="https://github1s.com/badaouiadampro-cpu/Portfolio" 
+            frameBorder="0" 
+            title="VsCode" 
+            className="h-full w-full bg-ub-cool-grey"
+        ></iframe>
     )
 }
 
 export const displayVsCode = () => {
-    <VsCode> </VsCode>
+    return <VsCode />
 }
